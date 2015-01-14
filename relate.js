97b161833a093f756fe8e535ec77d5aebd2bd0fe
wlaurance <w.laurance@gmail.com>
2015-01-14 15:54:21 -0500

    Build categories

--- relate.js
+++ relate.js
@@ -1,3 +1,12 @@
+var _ = require('underscore');
 module.exports = function(cats, rules, ruleqs) {
-  console.log(cats, rules, ruleqs);
+  var fCategories = {};
+  var fRules = {};
+  var fRuleQs = {};
+
+  _.each(cats, function(cat) {
+    fCategories[cat.id] = {
+      title: cat.title
+    };
+  });
 }
