71702180d517d66f76e783b44e051adbd2820c60
wlaurance <w.laurance@gmail.com>
2015-01-14 16:03:12 -0500

    Start generating rules and their classifications

--- relate.js
+++ relate.js
@@ -3,10 +3,33 @@ module.exports = function(cats, rules, ruleqs) {
   var fCategories = {};
   var fRules = {};
   var fRuleQs = {};
+  var classifications = [];
+
+  function checkOrAddClassification(c) {
+    var index = _.indexOf(classifications, c);
+    if (index !== -1) {
+      return index;
+    } else {
+      classifications.push(c);
+      return classifications.length - 1;
+    }
+  }
 
   _.each(cats, function(cat) {
     fCategories[cat.id] = {
-      title: cat.title
+      title: cat.title,
+      rules: []
     };
   });
+
+  _.each(rules, function(rule) {
+    fRules[rule.id] = {
+      title: rule.title,
+      description: rule.description,
+      ruleNumber: rule.id,
+      classification: checkOrAddClassification(rule.classification),
+      ruleQuestions: []
+    };
+  });
+  console.log(fRules);
 }
