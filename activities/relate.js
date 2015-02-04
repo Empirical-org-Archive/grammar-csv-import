bb47f4e18cb2b4f574f66fdaf7005b0e79b31b50
wlaurance <w.laurance@gmail.com>
2015-02-04 14:17:14 -0800

    Using a chaining operator

--- activities/relate.js
+++ activities/relate.js
@@ -1,18 +1,21 @@
 var _ = require('underscore');
-module.exports = function(activities, activityClassifications) {
+module.exports = function(activities) {
 
-  var sentenceWritings = _.groupBy(activities, 'activity_classification_id')[2];
-  var flags = [{id:1, name: 'production'}, {id:2, name: 'beta'}];
+  var flags = [{id:1, name: '{production'}, {id:2, name: '{beta}'},
+    {id:3, name: '{alpha}'}, {id:4, name:'{archived}'}
+  ];
 
-  sentenceWritings = _.map(sentenceWritings, function(n) {
-    return {
-      categoryId: n.topic_id,
-      description: n.description,
-      flags: n.flags,
-      data: n.data,
-      title: n.name,
-    };
-  });
+  var sentenceWritings = _.chain(_.groupBy(activities, 'activity_classification_id')[2])
+    .map(function(n) {
+      return {
+        categoryId: n.topic_id,
+        description: n.description,
+        flags: n.flags,
+        data: n.data,
+        title: n.name,
+      };
+    })
+    .value();
   require('build')(
     _.extend({}, sentenceWritings)
   );
