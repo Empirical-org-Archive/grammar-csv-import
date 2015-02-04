cd6b8896116a44d55305beb26c51147aae37a3c1
wlaurance <w.laurance@gmail.com>
2015-02-04 15:16:27 -0800

    Parse hstore
    If it doesn't parse, just throw it away

--- activities/relate.js
+++ activities/relate.js
@@ -1,9 +1,20 @@
 var _ = require('underscore');
+var hstore = require('hstore.js');
 module.exports = function(activities) {
 
   var flags = [{id:1, name: '{production}'}, {id:2, name: '{beta}'},
     {id:3, name: '{alpha}'}, {id:4, name:'{archived}'}
   ];
+
+  function makeRules(s) {
+    try {
+      var rules = hstore.parse(s).rule_position;
+      return rules;
+    } catch (e) {
+      return null;
+    }
+  }
+
 
   var sentenceWritings = _.chain(_.groupBy(activities, 'activity_classification_id')[2])
     .map(function(n) {
@@ -19,6 +30,11 @@ module.exports = function(activities) {
       f.flagId = _.findWhere(flags, {name: f.flags}).id;
       delete(f.flags);
       return f;
+    })
+    .map(function(d) {
+      d.rules = makeRules(d.data);
+      delete(d.data);
+      return d;
     })
     .value();
   require('build')(
