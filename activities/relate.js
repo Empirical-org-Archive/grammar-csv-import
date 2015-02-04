a4b9cbd52ee518399e13609e1d168cad6597aa7b
wlaurance <w.laurance@gmail.com>
2015-02-04 12:24:15 -0800

    Filter out sentence writings

--- activities/relate.js
+++ activities/relate.js
@@ -1,9 +1,19 @@
 var _ = require('underscore');
 module.exports = function(activities, activityClassifications) {
 
-  var sentenceWritings = {};
+  var sentenceWritings = _.groupBy(activities, 'activity_classification_id')[2];
+  var flags = [{id:1, name: 'production'}, {id:2, name: 'beta'}];
 
+  sentenceWritings = _.map(sentenceWritings, function(n) {
+    return {
+      categoryId: n.topic_id,
+      description: n.description,
+      flags: n.flags,
+      data: n.data,
+      title: n.name,
+    };
+  });
   require('build')(
-    sentenceWritings
+    _.extend({}, sentenceWritings)
   );
 }
