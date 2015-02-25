e64b174c39425ad123fcbd3295bab5e7a54520ec
wlaurance <w.laurance@gmail.com>
2015-02-25 14:37:47 -0800

    move regex to variable

--- activities/migrateIds.js
+++ activities/migrateIds.js
@@ -1,6 +1,8 @@
 var _ = require('underscore');
 var fs = require('fs');
 
+var regex = /{\+([^-]+)-([^|]+)\|([^}]+)}/g;
+
 module.exports = function(pfs) {
   var mapping = _.chain(fs.readFileSync('./updated_story_rules').toString().split('\n'))
     .filter(function(f) {
@@ -14,7 +16,7 @@ module.exports = function(pfs) {
   return _.chain(pfs)
     .map(function(pfs, key) {
       var passage = pfs.passage;
-      passage.replace(/{\+([^-]+)-([^|]+)\|([^}]+)}/g, function(key,b,c,ruleNumber) {
+      passage.replace(regex, function(key,b,c,ruleNumber) {
         if (mapping[ruleNumber]) {
           passage = passage.replace(key, key.replace(ruleNumber, mapping[ruleNumber]));
         }
