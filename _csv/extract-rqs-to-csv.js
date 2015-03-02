853e33daa2a6d3b603316fe83c16f0a13bce0a35
wlaurance <w.laurance@gmail.com>
2015-03-02 11:41:34 -0800

    Delimit things

--- _csv/extract-rqs-to-csv.js
+++ _csv/extract-rqs-to-csv.js
@@ -1,17 +1,19 @@
 var rqs = require('./../cms/cms.json').ruleQuestions;
+var rules = require('./../cms/cms.json').rules;
 var _ = require('underscore');
 
-if (!rqs) {
+if (!rqs || !rules) {
   throw new Error('What happened to the rule questions');
 }
-var exK = _.first(_.keys(rqs));
-var exR = _.union(_.keys(rqs[exK]), ['id']);
+var exR = ['ruleQuestionId', 'body', 'prompt'];
+rqs =_.map(rqs, function(v, k) {
+  v.ruleQuestionId = k;
+  v.body = JSON.stringify(v.body);
+  return v;
+})
 
 require('json2csv')({
-  data: _.map(rqs, function(v, k) {
-    v.id = k;
-    return v;
-  }),
+  data: rqs,
   fields: exR
 }, function(err, csv) {
   console.log(csv);
