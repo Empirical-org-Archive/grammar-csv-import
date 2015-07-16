63d63d0ecd9e517163e6db8a96c82245f15739b2
wlaurance <w.laurance@gmail.com>
2015-07-16 15:33:44 -0700

    nest rule questions into rules

--- v2/index.js
+++ v2/index.js
@@ -1,7 +1,9 @@
 var v1Activities = require(__dirname + './../v1/activities/activities.json');
 var v1Cms = require(__dirname + './../v1/cms/cms.json');
 
-var print = require(__dirname + './../print');
 
-print(v1Activities)
-print(v1Cms);
+var rules = v1Cms.rules;
+var ruleQuestions = v1Cms.ruleQuestions;
+
+var rulesWithNestedRuleQuestions = require('./mapRuleQuestionsToRules')(rules, ruleQuestions);
+require('../print')(rulesWithNestedRuleQuestions);
---
+++ v2/mapRuleQuestionsToRules.js
@@ -0,0 +1,14 @@
+var _ = require('underscore');
+module.exports = function(r, rq) {
+  console.log(rq);
+  return _.chain(r)
+    .map(function(rule) {
+      rule.ruleQuestions = _.map(rule.ruleQuestions, function(v, k) {
+        var foundRuleQuestion = rq[k];
+        foundRuleQuestion.id = k;
+        return foundRuleQuestion;
+      });
+      return rule;
+    })
+    .value();
+};
