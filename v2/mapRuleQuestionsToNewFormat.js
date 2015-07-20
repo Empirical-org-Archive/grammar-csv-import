f8bfddbd2ac698de62fb3845bbcb19b685ebda7e
wlaurance <w.laurance@gmail.com>
2015-07-20 11:27:20 -0700

    Build list of rule questions
    (ruleQuestionId, ruleQuestionAnswer(prompt, body), ruleId, concept_level_0)
    
    Next is calculating the new ruleID.

--- v2/mapRuleQuestionsToNewFormat.js
+++ v2/mapRuleQuestionsToNewFormat.js
@@ -1,4 +1,20 @@
 var _ = require('underscore');
 module.exports = function(rulesWithNestedRuleQuestions) {
-  return rulesWithNestedRuleQuestions;
+  var rqs = [];
+  _.each(rulesWithNestedRuleQuestions, function(rwnrq) {
+    var r = rwnrq;
+    _.map(r.ruleQuestions, function(rq) {
+      rq.ruleQuestionId = rq.id;
+      rq.ruleId = r.ruleNumber;
+      rq.concept_level_0 = rq.conceptTag;
+      rqs.push(_.omit(rq, [
+        'conceptCategory',
+        'conceptClass',
+        'conceptId',
+        'conceptTag',
+        'id'
+      ]));
+    });
+  });
+  return rqs;
 };
