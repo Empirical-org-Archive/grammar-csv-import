f5820f4116330bae247391ba0e33c93e63df41b9
wlaurance <w.laurance@gmail.com>
2015-09-01 16:04:33 -0700

    Add in lms standard_level

---
+++ v2/addLmsIds.js
@@ -0,0 +1,25 @@
+var Promise = require('bluebird');
+var http = require('request-promise-json');
+var host = 'https://staging.quill.org/api/v1';
+
+var _ = require('underscore');
+
+module.exports = function(conceptsWithQuestions) {
+  return Promise.props({
+    standards: http.get(host + '/topics'),
+    standard_levels: http.get(host + '/topic_categories'),
+    concepts: http.get(host + '/concepts')
+  }).then(function(result) {
+    var standards = result.standards.topic;
+    var standardLevels = result.standard_levels.topic_categories;
+    var concepts = result.concepts.concepts;
+    conceptsWithQuestions = _.chain(conceptsWithQuestions)
+      .map(function(c, id) {
+        c.standard_level = _.findWhere(standardLevels, {name: c.standard_level});
+        return [id, c];
+      })
+      .object()
+      .value();
+    return conceptsWithQuestions;
+  });
+};
