94aaf0a28ecfefa1ae3a23bbd02ef37d76ed07e3
wlaurance <w.laurance@gmail.com>
2015-09-01 15:19:21 -0700

    Add firebase ids to concept questions

--- v2/addFirebaseIds.js
+++ v2/addFirebaseIds.js
@@ -5,6 +5,12 @@ var fbId = require('./firebase-tools/generate-pushid');
 module.exports = function(conceptsWithQuestions) {
   return _.chain(conceptsWithQuestions)
     .map(function(c) {
+      c.questions = _.chain(c.questions)
+        .map(function(q) {
+          return [fbId(), q];
+        })
+        .object()
+        .value();
       return [fbId(), c];
     })
     .object()
