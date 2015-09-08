2df509ca594fc8010ae244803a5da6b35f5256db
wlaurance <w.laurance@gmail.com>
2015-09-08 14:52:39 -0700

    Return [id, rq] pair in new mapping
    When we move rule question to question, we can use the existing concept information.

--- v2/mapRuleQuestionsToNewFormat.js
+++ v2/mapRuleQuestionsToNewFormat.js
@@ -5,9 +5,6 @@ module.exports = function(rulesWithNestedRuleQuestions) {
     r.questions = _.map(r.questions, function(rq) {
       rq.ruleQuestionId = rq.id;
       rq.ruleId = r.ruleNumber;
-      rq.concept_level_2 = rq.conceptClass;
-      rq.concept_level_1 = rq.conceptCategory;
-      rq.concept_level_0 = rq.conceptTag;
       rq.instructions = require('./mapInstructionsBackIntoRuleQuestion')(rq);
       return _.omit(rq, [
         'conceptCategory',
