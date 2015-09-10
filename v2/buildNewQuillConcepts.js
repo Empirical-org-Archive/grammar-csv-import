257b271bc2ad7d15c40417616a86aa0bdd76cf15
wlaurance <w.laurance@gmail.com>
2015-09-09 17:57:05 -0700

    Parse new rule questions into object

--- v2/buildNewQuillConcepts.js
+++ v2/buildNewQuillConcepts.js
@@ -8,6 +8,12 @@ module.exports = function(rules, ruleQuestions, concepts) {
   }
 
   var questions = _.map(concepts, function(c) {
+    c.ruleQuestions = JSON.parse(
+      c.ruleQuestions
+        .replace(/""/g, '"')
+        .replace(/\\{2,}"\\\\"/g, '\\"')
+        .replace(/\\{1,}r\\{1,}n/g, '')
+    );
     var buddy = findRuleQuestionWithConcept(c);
     console.log(buddy.prompt, c.ruleQuestions);
     return buddy;
