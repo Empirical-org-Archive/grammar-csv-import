8f90418c589e79457cb315fd8eea87ee847f0dce
wlaurance <w.laurance@gmail.com>
2015-09-23 12:56:37 -0700

    Rebuild concept list for grammar activity

--- v2/index.grammarActivities.js
+++ v2/index.grammarActivities.js
@@ -3,8 +3,22 @@ var _ = require('underscore');
 var concepts = require(__dirname + '/./concepts.json');
 var oldGrammarActivities = require('./../v1/activities/activities.json').sentenceWritings;
 
+function findConceptUid(rn, index) {
+  var conceptKey = _.findKey(concepts, {ruleNumber: Number(rn)});
+  if (conceptKey) {
+    return conceptKey;
+  } else {
+    return 'needKey' + index;
+  }
+}
+
 function createConceptList(rules) {
-  return rules;
+  return _.chain(rules)
+    .map(function (r, index) {
+      return [findConceptUid(r.ruleId, index), r];
+    })
+    .object()
+    .value();
 }
 
 
